906414c773698bace53cd7feb8a4d367582d2c59
djarquinr101 <98566701+djarquinr101@users.noreply.github.com>
2023-02-14 22:59:56 -0600

    Update script.js
    JS code changed since I moved from analog clock to digital so the logic of the program needed to be changed

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
-const secondHand = document.querySelector('.second-hand');
-const minHand = document.querySelector('.min-hand');
-const hourHand = document.querySelector('.hour-hand');
+function displayTime(){
+    let dateTime = new Date();
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    let session = document.querySelector('#session');
 
+    hrs >= 12 ? session.innerHTML = 'PM': session.innerHTML = 'AM';
+    hrs > 12 ?  hrs = hrs -12: hrs = hrs;
 
-
-function setDate(){
-const now = new Date();
-
-const seconds = now.getSeconds();
-const secondsDegrees = ((seconds/60) * 360) + 90;
-secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
-
-const mins = now.getMinutes();
-const minsDegrees = ((mins/60) * 360) + 90;
-minHand.style.transform = `rotate(${minsDegrees}deg)`;
-
-const hours = now.getHours();
-const hoursDegrees = ((hours/60) * 360) + 90;
-hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
-
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
 }
 
-setInterval(setDate,1000);
+setInterval(displayTime,10)
